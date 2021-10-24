ef8784183abf522955a6c465a5d269f5477b5971
Piyush Agrawal <piyush200205@gmail.com>
2021-10-24 19:08:49 +0530

    added main

---
+++ cmake-build-debug/CMakeFiles/Trees_Algorithms.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Trees_Algorithms.dir/main.cpp.obj"
+  "Trees_Algorithms.exe"
+  "Trees_Algorithms.exe.manifest"
+  "Trees_Algorithms.pdb"
+  "libTrees_Algorithms.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Trees_Algorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
