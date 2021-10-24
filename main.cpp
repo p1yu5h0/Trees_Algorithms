00c0dc44c7d3ef8ec81eca19c5a1b67d7490dc59
Piyush Agrawal <piyush200205@gmail.com>
2021-10-24 20:24:12 +0530

    added preorder-recursive

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class node{
+public:
+    int data;
+    node* left;
+    node* right;
+    node(int val){
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+void preorderrecursive(node* root){
+    if(root == nullptr){
+        return;
+    }
+    cout<<root->data;
+    preorderrecursive(root->left);
+    preorderrecursive(root->right);
+}
+
 int main() {
-    cout << "Hello, World!" << endl;
+    node* root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->right = new node(5);
+
     return 0;
 }
