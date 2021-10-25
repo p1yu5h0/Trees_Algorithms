c2fce5f5cb302c5e319caf3337fcad54d84bb042
Piyush Agrawal <piyush200205@gmail.com>
2021-10-25 08:19:29 +0530

    Added Level Order Traversal.

--- main.cpp
+++ main.cpp
@@ -39,6 +39,44 @@ void postorderrecursive(node* root){
     cout<<root->data;
 }
 
+vector<vector<int>> levelOrder(node* root) {
+    /*
+     * Algorithm:-
+     * To store the level order traversal level wise we need a queue data structure and a 2d array
+     * Each of the row of the 2d array contains the value of all the nodes in that level
+     * Queue ds initially have the root in it
+     * Now we start traversing over the queue and take out the front node first(initially the root node)
+     * Now if left of that node exists we push that inside the queue and then if the right exists, we push that too inside the queue
+     * After completing all the nodes of that level and putting their children inside the queue, we push all the parents on that level inside a vector
+     * In the end of that level we push that level array (1D) inside our ans array (2D)
+     * We do this process for all the nodes ie. till the queue finally become empty
+     * In the end the answer 2D vector is the final level order or bfs traversal of the given tree
+     * */
+    vector<vector<int>> ans;
+    if(root == nullptr){
+        return ans;
+    }
+    queue<node*> q;
+    q.push(root);
+    while(!q.empty()){
+        int size = q.size();
+        vector<int> level;
+        while(size--){
+            node* temp = q.front();
+            q.pop();
+            if(temp->left!=nullptr){
+                q.push(temp->left);
+            }
+            if(temp->right!=nullptr){
+                q.push(temp->right);
+            }
+            level.push_back(temp->data);
+        }
+        ans.push_back(level);
+    }
+    return ans;
+}
+
 int main() {
     node* root = new node(1);
     root->left = new node(2);
